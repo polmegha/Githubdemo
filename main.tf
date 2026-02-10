cd07453a1bc9e68d2d55914cc264a253925a9ba2
Megha Pol <meghapol199@gmail.com>
2026-02-10 22:06:41 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+#resource group
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "rg-dev-002"
+  location = "centralindia"
+}
+
+#storage 
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfrgdev002"   # must be globally unique
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  # enable_https_traffic_only = true
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
+
+ #Virtual Network 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-dev-002"
+  address_space       = ["10.10.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  tags                = var.tags
+}
---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "resource_group_name" {}
+variable "location" {}
+
+variable "vnet_name" {}
+variable "vnet_address_space" {}
+
+# variable "subnet_name" {}
+# variable "subnet_address_prefix" {}
+
+variable "tags" {
+  type = map(string)
+}
