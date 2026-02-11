660be10e0f107529ca5d15443f478d762dbcbf10
Megha Pol <meghapol199@gmail.com>
2026-02-11 13:16:11 +0530

    remove RG

--- main.tf
+++ main.tf
@@ -10,40 +10,63 @@ terraform {
 provider "azurerm" {
   features {}
 }
-#resource group
+# #resource group
 
-resource "azurerm_resource_group" "rg1" {
-  name     = "rg-dev-002"
-  location = "centralindia"
-}
-# resource "azurerm_resource_group" "rg2" {
-#   name     = "rg-dev-003"
+# resource "azurerm_resource_group" "rg1" {
+#   name     = "rg-dev-002"
 #   location = "centralindia"
 # }
 
-#storage 
+
+# #storage 
+# resource "azurerm_storage_account" "tfstate" {
+#   name                     = "tfrgdev002"   # must be globally unique
+#   resource_group_name      = azurerm_resource_group.rg1.name
+#   location                 = var.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#   # enable_https_traffic_only = true
+
+#   tags = var.tags
+# }
+
+# resource "azurerm_storage_container" "tfstate" {
+#   name                  = "tfstate"
+#   storage_account_name  = azurerm_storage_account.tfstate.name
+#   container_access_type = "private"
+# }
+
+#  #Virtual Network 
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = "vnet-dev-002"
+#   address_space       = ["10.10.0.0/16"]
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.rg1.name
+#   tags                = var.tags
+# }
+
+#---------------new
+
+# Use existing Resource Group
+data "azurerm_resource_group" "rg1" {
+  name = var.resource_group_name
+}
+
+# Storage account
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfrgdev002"   # must be globally unique
-  resource_group_name      = azurerm_resource_group.rg1.name
-  location                 = var.location
+  name                     = "tfrgdev002"
+  resource_group_name      = data.azurerm_resource_group.rg1.name
+  location                 = data.azurerm_resource_group.rg1.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  # enable_https_traffic_only = true
-
-  tags = var.tags
+  tags                     = var.tags
 }
 
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
-}
-
- #Virtual Network 
+# VNet
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-dev-002"
-  address_space       = ["10.10.0.0/16"]
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg1.name
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = data.azurerm_resource_group.rg1.location
+  resource_group_name = data.azurerm_resource_group.rg1.name
   tags                = var.tags
 }
