e96e6e4f1ceadce681fb66eaaa761cdc6decfc91
Megha Pol <meghapol199@gmail.com>
2026-02-11 15:04:03 +0530

    update subnet

--- variable.tf
+++ variable.tf
@@ -4,8 +4,17 @@ variable "location" {}
 variable "vnet_name" {}
 variable "vnet_address_space" {}
 
-variable "subnet_name" {}
-variable "subnet_address_prefix" {}
+variable "subnet_name" {
+  type = string
+}
+
+variable "subnet_address_prefix" {
+  type = list(string)
+}
+
+
+# variable "subnet_name" {}
+# variable "subnet_address_prefix" {}
 
 variable "tags" {
   type = map(string)
